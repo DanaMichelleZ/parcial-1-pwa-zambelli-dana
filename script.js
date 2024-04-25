56f446b6941af730d4814a1fb82ae472ac5b069a
DanaMichelleZ <dana.zambelli@davinci.edu.ar>
2024-04-25 18:34:13 -0300

    estructura
    avanzo con la estructura base siguiendo la clase paso a paso

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const URL_ENDOPOINT = 'https://pokeapi.co/api/v2/';
-
 const URL_POKEMONES = URL_ENDOPOINT + 'pokemon';
+
+const mostrarPokemon = (pokemon) => {
+    console.log(pokemon)
+    const pokedex = document.getElementById("pokedex")
+
+    const li = document.createElement('li');
+
+    const titulo = document.createElement('h3');
+    li.appendChild(titulo);
+
+    const descripcion = document.createElement('p');
+    li.appendChild(descripcion);
+
+    pokedex.appendChild(li);
+    console.log(pokedex);
+}
 
 fetch(URL_POKEMONES)
 .then(data => data.json())
 .then(result => {
-    const results = result.results;
+    const results = result.results; //este es el array de objetos q devuelve en la consola del nav//
+    const primerResultado = results [10]; // results es el objeto en cuestion//
 
-    console.log("lista de pokemones", results)
+    mostrarPokemon(primerResultado)
 })
